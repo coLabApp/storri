9769fa9482b0e0ad9d4e67b5db6812a4bd39aa44
Abidu1 <abidusra@gmail.com>
2021-03-14 18:08:02 -0400

    updated

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 const cardBoard = document.querySelector('.indexCardBoard');
-        const btns = document.querySelectorAll('button');
+        // const btns = document.querySelectorAll('button');
         const menu = document.querySelector('ul');
+        const newBtn = document.querySelector('.new')
 
-        btns.forEach(btn =>{
-            btn.addEventListener('click', (e) => {
-                const target = e.target.classList
-                if(target.contains('Who')){
-                    createPrompt("Who")
-                }else if(target.contains('What')){
-                    createPrompt("What")
-                }else if(target.contains('When')){
-                    createPrompt("When")
-                }else if(target.contains('Why')){
-                    createPrompt("Why")
-                }else if(target.contains('How')){
-                    createPrompt("How")
-                }else if(target.contains('Where')){
-                    createPrompt('Where')
-                }else{
-                    showMenu();
+        // btns.forEach(btn =>{
+        //     btn.addEventListener('click', (e) => {
+        //         const target = e.target.classList
+        //         if(target.contains('Who')){
+        //             createPrompt("Who")
+        //         }else if(target.contains('What')){
+        //             createPrompt("What")
+        //         }else if(target.contains('When')){
+        //             createPrompt("When")
+        //         }else if(target.contains('Why')){
+        //             createPrompt("Why")
+        //         }else if(target.contains('How')){
+        //             createPrompt("How")
+        //         }else if(target.contains('Where')){
+        //             createPrompt('Where')
+        //         }else{
+        //             showMenu();
                     
-                }
+        //         }
                 
-            })
-        });
+        //     })
+        // });
         
- 
+    
+        newBtn.addEventListener('click', createPrompt);
 
-        function createPrompt (section){
+        function createPrompt (){
             const card = document.createElement('div');
+            card.classList.add('openModal')
             card.innerHTML = 
-            `<div class="cardContainer">
+            `<div class="cardContainer active">
                 <div class="tools">
-                    <button class="edit"><i class="fas fa-edit"></i></button>
+                    <button class="fullScreen"><i class="fas fa-expand-alt"></i></button>
                     <button class="delete"><i class="fas fa-trash-alt"></i></button>
                 </div>
-                <h1>${section}</h1>
                 <textarea class="card" placeholder="write here"></textarea>
             <div>`
-            const editBtn = card.querySelector('.edit');
+            const fullScreen = card.querySelector('.fullScreen');
             const deleteBtn = card.querySelector('.delete');
+            const cardContainer = card.querySelector('.cardContainer')
             cardBoard.appendChild(card)
+
+            fullScreen.addEventListener('click', function () {
+                card.classList.toggle('openModal')
+                cardContainer.classList.toggle('active')
+                
+            })
 
             deleteBtn.addEventListener('click', function () {
                 card.remove()
