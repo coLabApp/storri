76600215f461fafa4c6ecc7b5af728c91da01a24
Abidu1 <abidusra@gmail.com>
2021-03-15 04:13:15 -0400

    drag and drop

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const menu = document.querySelector('ul');
 const newBtn = document.querySelector('.new')
 const scenes = document.querySelectorAll('.scene')
 
+
+
+
         // btns.forEach(btn =>{
         //     btn.addEventListener('click', (e) => {
         //         const target = e.target.classList
@@ -31,9 +34,9 @@ const scenes = document.querySelectorAll('.scene')
         newBtn.addEventListener('click', createPrompt);
 
         function createPrompt (){
-            const card = document.createElement('div');
-            card.classList.add('openModal')
-            card.innerHTML = 
+            const cards = document.createElement('div');
+            cards.classList.add('openModal')
+            cards.innerHTML = 
             `<div class="cardContainer active">
                 <div class="tools">
                     <button class="fullScreen"><i class="fas fa-expand-alt"></i></button>
@@ -45,17 +48,17 @@ const scenes = document.querySelectorAll('.scene')
                 <button class="saveBtn">Save</button>
             <div>`
 
-            card.setAttribute('draggable' , true)
-            const fullScreen = card.querySelector('.fullScreen');
-            const deleteBtn = card.querySelector('.delete');
-            const cardContainer = card.querySelector('.cardContainer');
-            const saveBtn = card.querySelector('.saveBtn');
-            const textArea = card.querySelector('textarea')
-            cardBoard.appendChild(card)
+            cards.setAttribute('draggable' , true)
+            const fullScreen = cards.querySelector('.fullScreen');
+            const deleteBtn = cards.querySelector('.delete');
+            const cardContainer = cards.querySelector('.cardContainer');
+            const saveBtn = cards.querySelector('.saveBtn');
+            const textArea = cards.querySelector('textarea')
+            cardBoard.appendChild(cards)
 
             saveBtn.addEventListener('click', function () {
                 textArea.classList.toggle('textScreen')
-                card.classList.toggle('openModal')
+                cards.classList.toggle('openModal')
                 cardContainer.classList.toggle('active')
                 saveBtn.classList.add('hideDiv')
                 
@@ -63,22 +66,66 @@ const scenes = document.querySelectorAll('.scene')
 
             fullScreen.addEventListener('click', function () {
                 textArea.classList.toggle('textScreen')
-                card.classList.toggle('openModal')
+                cards.classList.toggle('openModal')
                 cardContainer.classList.toggle('active')
                 saveBtn.classList.remove('hideDiv');
                 
             })
 
             deleteBtn.addEventListener('click', function () {
-                card.remove()
+                cards.remove()
             })
-        } 
 
+            // const cardsSection = cards.querySelectorAll('.cards')
+            
+            // cardsSection.forEach(item =>{
+            //     item.addEventListener('click', (e) =>{
+            //         console.log(e.current.target)
+            //     })
+            // })
+
+            // cards.querySelectorAll('.cards').forEach(card =>{
+                cards.addEventListener('dragstart', dragStart)
+                cards.addEventListener('dragend', dragEnd)
+
+                for(const scene of scenes) {
+                    scene.addEventListener('dragover', dragOver)
+                    scene.addEventListener('dragenter', dragEnter)
+                    scene.addEventListener('dragleave', dragLeave)
+                    scene.addEventListener('drop', dragDrop)
+                }
+                
+                function dragStart(e) {
+                    e.currentTarget += ' hold' 
+                }
+                
+                function dragEnd() {
+                    this.className = 'fill'
+                }
+                
+                function dragOver(e) {
+                    e.preventDefault()
+                }
+                
+                function dragEnter(e) {
+                    e.preventDefault()
+                    this.className += ' hovered'
+                }
+                
+                function dragLeave() {
+                    this.className = 'scene'
+                }
+                
+                function dragDrop(e) {
+                    this.className = 'scene'
+                    this.append(cards)
+                }
+                  
+            }
+          
         function showMenu(){
             menu.classList.toggle('active');
             
         }
 
-        function dragStart(){
-
-        }
+       
